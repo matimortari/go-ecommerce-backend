3ca8dc3881aad91e324b06432b0f59f1a209db7f
matheus <matheus.felipe.19rt@gmail.com>
2025-01-05 15:45:56 -0300

    test: implement testing for routes package | chore: add payload validation for user registration

--- go.mod
+++ go.mod
@@ -9,3 +9,14 @@ require (
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.31.0
 )
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.23.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
--- services/user/routes.go
+++ services/user/routes.go
@@ -3,7 +3,9 @@ package user
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 	"github.com/matimortari/go-backend/services/auth"
 	"github.com/matimortari/go-backend/types"
@@ -35,6 +37,13 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 		utils.WriteError(w, http.StatusBadRequest, err)
 	}
 
+	// Validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %s", errors))
+		return
+	}
+
 	// Check if the user exists
 	_, err := h.store.GetUserByEmail(payload.Email)
 	if err == nil {
@@ -61,9 +70,28 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	utils.WriteJSON(w, http.StatusCreated, nil)
+		utils.WriteJSON(w, http.StatusCreated, nil)
+	}
 
-	// Process the payload
-	
-	// Continue with registration logic
+func (h *Handler) handleGetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	str, ok := vars["userID"]
+	if !ok {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("missing user ID"))
+		return
+	}
+
+	userID, err := strconv.Atoi(str)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid user ID"))
+		return
+	}
+
+	user, err := h.store.GetUserByID(userID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, user)
 }
